eca6c334f7321a478f07601d73c91ed58a8101e2
yangk <yangk@goodrain.com>
2022-09-18 15:50:13 +0800

    test log output

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,15 @@
-FROM golang:1.17 as go
+FROM yangk/containerd-test:v0.0.1 as go
 WORKDIR /workspace
 COPY . /workspace
-RUN apt update \
-    && apt -y install bats golang-github-containerd-btrfs-dev git libapparmor-dev libdevmapper-dev libglib2.0-dev libgpgme11-dev libseccomp-dev libselinux1-dev go-md2man \
-    && go mod vendor && CGO_ENABLED=1 go build -o main main.go
+ENV GOPROXY=https://goproxy.cn
+RUN go mod vendor && CGO_ENABLED=1 go build -o main main.go
 
-ARG FILEDIR=/workspace
-ENV CONTEXT_DIR=${FILEDIR}
-ENV TRANSIENT_MOUNT=${FILEDIR}:${FILEDIR}
-ENV DOCKERFILE_NAME=${FILEDIR}/Dockerfile.test
-ENV OUTPUT=docker.io/yangk/builah-test:0.0.1
-CMD ["/workspace/main"]
+#ARG FILEDIR=/workspace
+#ENV CONTEXT_DIR=${FILEDIR}
+#ENV TRANSIENT_MOUNT=${FILEDIR}:${FILEDIR}
+#ENV DOCKERFILE_NAME=${FILEDIR}/Dockerfile.test
+#ENV OUTPUT=docker.io/yangk/builah-test:0.0.1
+#CMD ["/workspace/main"]
 
 #FROM goodrainapps/alpine:3.4
 #WORKDIR /root
--- main.go
+++ main.go
@@ -20,67 +20,7 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
-	_, runtimeConn, err := criutil.GetRuntimeClient(&ctx)
-	if err != nil {
-		logrus.Errorf("get runtime client failed %v", err)
-		return
-	}
-	defer criutil.CloseConnection(runtimeConn)
-
-	if buildah.InitReexec() {
-		return
-	}
-	unshare.MaybeReexecUsingUserNamespace(false)
-
-	output := &bytes.Buffer{}
-	options := define.BuildOptions{
-		ContextDirectory: os.Getenv("CONTEXT_DIR"),
-		CommonBuildOpts:  &define.CommonBuildOptions{},
-		TransientMounts:         []string{os.Getenv("TRANSIENT_MOUNT")},
-		Output:                  os.Getenv("OUTPUT"),
-		OutputFormat:            buildah.Dockerv2ImageManifest,
-		Out:                     output,
-		Err:                     output,
-		Layers:                  true,
-		NoCache:                 true,
-		RemoveIntermediateCtrs:  true,
-		ForceRmIntermediateCtrs: true,
-	}
-	storeOptions := storage.StoreOptions{}
-	store, err := storage.GetStore(storeOptions)
-	logrus.Info("Get Store Start")
-	if err !=nil{
-		logrus.Errorf("Get Store failed: %+v", err)
-		os.Exit(1)
-	}
-	logrus.Info("Get Store Success")
-	// build the image and gather output. log the output if the build part of the test failed
-	imageID, imageName, err := imagebuildah.BuildDockerfiles(ctx,store , options, os.Getenv("DOCKERFILE_NAME"))
-	if err != nil {
-		logrus.Errorf("Build err %v", err)
-		output.WriteString("\n" + err.Error())
-		os.Exit(1)
-	}
-	logrus.Info("Build Success")
-	outputString := output.String()
-	logrus.Infof("imageID [%s] \nout [%s]", imageID, outputString)
-	logrus.Infof("imageName : [%s] imageName.String() [%s]", imageName.Name(), imageName.String())
-	dest, err := alltransports.ParseImageName(imageName.String())
-	if err !=nil{
-		logrus.Errorf("Parse image name err %v", err)
-		os.Exit(1)
-	}
-	logrus.Infof("dest : [%s]", dest)
-	ref, digest, err :=buildah.Push(context.Background(), imageName.Name(), dest, buildah.PushOptions{})
-	if err !=nil{
-		logrus.Errorf("Push image name err %v", err)
-		os.Exit(1)
-	}
-	logrus.Infof("ref is %+v", ref)
-	if ref != nil {
-		logrus.Infof("pushed image %q with digest %s", ref, digest.String())
-	}
+	getContainerStatus()
 
 	//pullImage(err, ctx)
 
@@ -119,6 +59,70 @@ func main() {
 	//defer client.Close()
 }
 
+func buildahPush() {
+	ctx := context.Background()
+	_, runtimeConn, err := criutil.GetRuntimeClient(&ctx)
+	if err != nil {
+		logrus.Errorf("get runtime client failed %v", err)
+		return
+	}
+	defer criutil.CloseConnection(runtimeConn)
+
+	if buildah.InitReexec() {
+		return
+	}
+	unshare.MaybeReexecUsingUserNamespace(false)
+
+	output := &bytes.Buffer{}
+	options := define.BuildOptions{
+		ContextDirectory:        os.Getenv("CONTEXT_DIR"),
+		CommonBuildOpts:         &define.CommonBuildOptions{},
+		TransientMounts:         []string{os.Getenv("TRANSIENT_MOUNT")},
+		Output:                  os.Getenv("OUTPUT"),
+		OutputFormat:            buildah.Dockerv2ImageManifest,
+		Out:                     output,
+		Err:                     output,
+		Layers:                  true,
+		NoCache:                 true,
+		RemoveIntermediateCtrs:  true,
+		ForceRmIntermediateCtrs: true,
+	}
+	storeOptions := storage.StoreOptions{}
+	store, err := storage.GetStore(storeOptions)
+	logrus.Info("Get Store Start")
+	if err != nil {
+		logrus.Errorf("Get Store failed: %+v", err)
+		os.Exit(1)
+	}
+	logrus.Info("Get Store Success")
+	// build the image and gather output. log the output if the build part of the test failed
+	imageID, imageName, err := imagebuildah.BuildDockerfiles(ctx, store, options, os.Getenv("DOCKERFILE_NAME"))
+	if err != nil {
+		logrus.Errorf("Build err %v", err)
+		output.WriteString("\n" + err.Error())
+		os.Exit(1)
+	}
+	logrus.Info("Build Success")
+	outputString := output.String()
+	logrus.Infof("imageID [%s] \nout [%s]", imageID, outputString)
+	logrus.Infof("imageName : [%s] imageName.String() [%s]", imageName.Name(), imageName.String())
+	dest, err := alltransports.ParseImageName(imageName.String())
+	if err != nil {
+		logrus.Errorf("Parse image name err %v", err)
+		os.Exit(1)
+	}
+	logrus.Infof("dest : [%s]", dest)
+	ref, digest, err := buildah.Push(context.Background(), imageName.Name(), dest, buildah.PushOptions{})
+	if err != nil {
+		logrus.Errorf("Push image name err %v", err)
+		os.Exit(1)
+	}
+	logrus.Infof("ref is %+v", ref)
+	if ref != nil {
+		logrus.Infof("pushed image %q with digest %s", ref, digest.String())
+	}
+}
+
 func pullImage(err error, ctx context.Context) {
 	imageClient, imageConn, err := criutil.GetImageClient(&ctx)
 	if err != nil {
@@ -140,13 +144,13 @@ func pullImage(err error, ctx context.Context) {
 	logrus.Println(resp.String())
 }
 
-func getContainerStatus(err error) {
+func getContainerStatus() {
 	runtimeService, err := remote.NewRemoteRuntimeService(criutil.RuntimeEndpoint, time.Second*3)
 	if err != nil {
 		logrus.Errorf("New Remote Runtime Service %v", err)
 		return
 	}
-	status, err := runtimeService.ContainerStatus("8acbeb3d0c3a")
+	status, err := runtimeService.ContainerStatus("0315ab64f")
 	if err != nil {
 		logrus.Errorf("Get Container Status %v", err)
 		return
